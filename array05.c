b946c092c42862b5f6c32086885ecae02eb69ff0
Patel Hit <patelhit2058@gmail.com>
2025-07-19 11:55:10 +0530

    Update array05.c

--- array05.c
+++ array05.c
@@ -1,18 +1,24 @@
 /* Swapping */
 #include <stdio.h>
-#define size 10
 void main()
 {
-	int number[size] = {78,17,91,16,0,54,37,7,99,49},i,temp;
+	int number[10],temp;
+    printf("Enter 10 numbers :");
+    for(int i=0;i<10;i++)
+    {
+        scanf("%d",&number[i]);
+    }
 	printf("\nBefore swapping\n");
-	for(i=0; i<size; ++i)printf("%d\t",number[i]);
-	
-	for(i=0; i<size; i+=2)
+	for(int i=0; i<10; i++)
+    {
+      printf("%d\t",number[i]);
+    }
+	for(int i=0; i<10; i+=2)
 	{
 		temp = number[i];
 		number[i]=number[i+1];
 		number[i+1]=temp;
 	}
 	printf("\nAfter swapping\n");
-	for(i=0; i<size; ++i)printf("%d\t",number[i]);
+	for(int i=0; i<10; ++i)printf("%d\t",number[i]);
 }
