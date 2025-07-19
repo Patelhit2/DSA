c57d57bd784b18514b7febabf29ddb76ede67770
Patel Hit <patelhit2058@gmail.com>
2025-07-19 11:49:13 +0530

    Update array01.c

--- array01.c
+++ array01.c
@@ -1,18 +1,19 @@
-/* Static Memory Allocation */
-/* Count total number of pass/fail students along with their percentage */
-#include <stdio.h>
-#define size 10
-void main()
-{
-	int marks[size] = {78,45,91,16,0,54,49,7,99,11};
-	int i, countPass=0, countFail=0;
-	for(i=0; i<size; ++i)
-	{
-		if(marks[i]>=35) {countPass++;}
-		else {countFail++;}
-	}
-	printf("Pass students = %d\n", countPass);
-	printf("Fail students = %d\n", countFail);
-	printf("Percentage of Pass Students = %f\n",(countPass/(size*1.0))*100.0);
-	printf("Percentage of Fail Students = %f\n",((countFail*1.0)/size)*100.0);
+#include<stdio.h>
+int main(){
+int a[10];
+printf("enter the elements of array :");
+ for(int i=0;i<=9;i++){
+     scanf("%d", &a[i]);
+ }
+for(int i=0;i<=9;i++){
+    printf("%d\n",a[i]);
+}
+int sum=0;
+for(int i=0;i<=9;i++){
+    sum+=a[i];
+}
+float avg=sum/10;
+printf("The average of the array element is %f.",avg);
+
+ return 0;
 }
